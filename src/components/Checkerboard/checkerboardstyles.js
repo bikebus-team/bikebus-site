497e2588e020dd9d4be6b1ba847a31fdfc2bd24b
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-10-29 08:18:46 -0400

    Checkerboard created and added to Story page, css is off

---
+++ src/components/Checkerboard/checkerboardstyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { ComponentWrapper } from "../Base/basecomponents";
+
+export const CheckerboardWrapper = styled(ComponentWrapper)`
+display: flex;
+`;
+
+export const TextWrapper = styled.div `
+padding: 10px;
+width: 60%;
+`;
