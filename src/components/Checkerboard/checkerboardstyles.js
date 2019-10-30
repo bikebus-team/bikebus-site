1c4746928e17c29e450f266cc6faf24b7545ecee
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-10-30 17:44:14 -0400

    Removed extraneous padding

--- src/components/Checkerboard/checkerboardstyles.js
+++ src/components/Checkerboard/checkerboardstyles.js
@@ -4,11 +4,7 @@ import { ComponentWrapper } from "../Base/basecomponents";
 export const CheckerboardWrapper = styled(ComponentWrapper)`
 display: flex;
 flex-direction: ${props => props.imageLeft ? "row" : "row-reverse"};
-padding-left: 0px
-padding-right: 0px;
 `;
 export const TextWrapper = styled.div `
 width: 60%;
-padding-left: 0px
-padding-right: 0px;
 `;
