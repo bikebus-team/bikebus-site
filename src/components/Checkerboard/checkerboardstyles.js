9155aec87e74d347f11d8476006166cafedbf1bc
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-11-03 17:10:18 -0500

    Added button functionality for checkerboards, basic page title, added loop to add checkerboards in an array, modified css

--- src/components/Checkerboard/checkerboardstyles.js
+++ src/components/Checkerboard/checkerboardstyles.js
@@ -1,10 +1,27 @@
 import styled from "styled-components";
-import { ComponentWrapper } from "../Base/basecomponents";
+import { Button, ComponentWrapper, Link } from "../Base/basecomponents";
 
 export const CheckerboardWrapper = styled(ComponentWrapper)`
-display: flex;
-flex-direction: ${props => props.imageLeft ? "row" : "row-reverse"};
+    display: inline-flex;
+    flex-flow: row;
+    flex-direction: ${props => props.imageLeft ? "row" : "row-reverse"};
+    margin: 1% 10%
+    align-items: center;
 `;
-export const TextWrapper = styled.div `
-width: 60%;
+
+export const RightCheckerboardWrapper = styled.div`
+    padding-left: ${props => props.imageLeft ? "10%" : "0%"};
+    padding-right: ${props => props.imageLeft ? "0%" : "5%"};
+`;
+
+export const TextWrapper = styled.div`
+    width: 80%;
+`;
+
+export const CheckerboardButton = styled(Button)`
+    display: ${props => props.display ? "block" : "none"};
+`;
+
+export const CheckerboardLink = styled(Link)`
+    text-decoration: none;
 `;
---
+++ src/components/PageTitle/pagetitlestyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { ComponentWrapper } from "../Base/basecomponents";
+
+export const PageTitleWrapper = styled(ComponentWrapper)`
+    background-color: gray;
+    width: 100vw;
+    height: 25vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TextWrapper = styled.div`
+    color: white;
+`;
