5b79d2ab6bb154cb9a16942ea0f12612c40f0095
Melvin <chen.melv@husky.neu.edu>
2019-11-10 08:26:04 -0500

    working cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -27,6 +28,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+          typeName: "TS",
+          fieldName: "takeshape",
+          // Url to query from
+          url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
+          // HTTP headers
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
+          },
+          // Additional options to pass to node-fetch
+          fetchOptions: {},
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,86 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import {
-  Button
-} from "../components/Base/basecomponents";
+import { Button } from "../components/Base/basecomponents"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>{data.takeshape.getHomepage.hero.description}</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Button>TestButton</Button>
     <Link to="/page-2/">Go to page 2</Link>
+    {console.log(data)}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    takeshape {
+      getHomepage(_id: "4f41b6ff-8b04-4e0f-8cc9-d5deaced26e4") {
+        _id
+        checkerboard1 {
+          buttonLink
+          buttonTitle
+          description
+          title
+        }
+        hero {
+          backgroundImage {
+            _id
+            caption
+            credit
+            description
+            filename
+            mimeType
+            path
+            sourceUrl
+            title
+            uploadStatus
+          }
+          description
+          headline
+        }
+        quoteSection {
+          quoteItem {
+            backgroundImage {
+              _id
+              caption
+              credit
+              description
+              filename
+              mimeType
+              path
+              sourceUrl
+              title
+              uploadStatus
+            }
+            quote
+          }
+        }
+        sock {
+          description
+          title
+        }
+        specialEvent {
+          dateTime
+          description
+          link
+          location
+          title
+        }
+      }    
+    }
+  }
+`
