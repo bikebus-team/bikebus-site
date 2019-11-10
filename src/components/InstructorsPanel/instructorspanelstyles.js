4223b5938a37fbe2a463fee0bdde5f83d0771a6e
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-11-10 17:24:09 -0500

    Added instructors component and padded wrapper. Checkerboard now uses padded wrapper and changes CSS dynamically

---
+++ src/components/InstructorsPanel/instructorspanelstyles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { PaddedComponentWrapper } from "../Base/basecomponents";
+
+export const InstructorsPanelWrapper = styled(PaddedComponentWrapper)`
+    flex-direction: column;
+`;
+
+export const InstructorsWrapper = styled.div`
+    display: flex;
+    flex-direction: row
+    justify-content: space-between;
+    
+    @media (max-width: 768px) {
+        flex-direction: column
+    }
+`;
+
+export const SingleInstructorWrapper = styled.div`
+    width: 30%
+    text-align: center
+
+    @media (max-width: 768px) {
+        width: 100%
+    }
+`;
+
+export const TextWrapper = styled.div`
+`;
--- src/pages/story.js
+++ src/pages/story.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Checkerboard from "../components/Checkerboard/checkerboard"
 import PageTitle from "../components/PageTitle/pagetitle"
+import InstructorsPanel from "../components/InstructorsPanel/instructorspanel"
 
 const mockJSONData = {
   data: {
@@ -21,7 +22,8 @@ const mockJSONData = {
         title: "Title3",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       }
-    ]
+    ],
+    instructorsTitle: "Meet our Instructors!"
   }
 }
 
@@ -33,11 +35,11 @@ const Story = () => (
     {mockJSONData.data.checkerboards.map((cb, index) => 
       <Checkerboard 
         key={index} 
-        imageLeft={index % 2 == 1} 
+        imageLeft={index % 2 === 1} 
         title={cb.title} 
         text={cb.text} 
         hasButton={false} />)}
-    <Link to="/">Go back to the homepage</Link>
+    <InstructorsPanel title={mockJSONData.data.instructorsTitle}/>
   </Layout>
 )
 
