86fd4f0601a5482d350b421d971d6d9f828c032f
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-12-11 13:30:00 -0500

    fixed bug for if a package was not preselected it would fail

--- src/pages/form.js
+++ src/pages/form.js
@@ -4,9 +4,10 @@ import SEO from "../components/seo";
 import FormComponent from "../components/FormComponent/formcomponent";
 
 const Form = ({ location }) => {
+    let clickedOption = location.state ? location.state.option : 0;
     return <Layout>
       <SEO title="Form" />
-      <FormComponent clickedOption={location.state.option} />
+      <FormComponent clickedOption={clickedOption} />
     </Layout>
 }
   
