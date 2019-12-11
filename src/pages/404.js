87bd4b601f6f91bb5ca18f7a2e04cb0d8b092c4f
Jean Zhang <jean.nmn.zhang@gmail.com>
2019-12-11 16:29:07 -0500

    bug fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { BB1, BBp } from "../components/Base/fonts";
+import { BBh1, BBp } from "../components/Base/fonts";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <BB1>NOT FOUND</BB1>
+    <BBh1>NOT FOUND</BBh1>
     <BBp>You just hit a route that doesn&#39;t exist... the sadness.</BBp>
   </Layout>
 )
