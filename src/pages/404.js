6506bbeb3677ab342fe62c9a184c0a5e40f17d69
Melvin <chen.melv@husky.neu.edu>
2019-12-15 20:58:08 -0800

    404 styling and removing a console log

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,21 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { BBh1, BBp } from "../components/Base/fonts";
+import { BBh2, BBp } from "../components/Base/fonts"
+import { FOFHero, FOFLink, FOFWrapper } from "../components/FOFHero/FOFStyles"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <BBh1>NOT FOUND</BBh1>
-    <BBp>You just hit a route that doesn&#39;t exist... the sadness.</BBp>
+    <FOFWrapper>
+      <FOFHero>404</FOFHero>
+      <BBh2>This page was not found</BBh2>
+      <BBp>
+        It looks like you took a wrong turn; this page does not exist yet! Would
+        you like to&nbsp;
+        <FOFLink to={"./"}>return to the homepage</FOFLink>?
+      </BBp>
+    </FOFWrapper>
   </Layout>
 )
 
